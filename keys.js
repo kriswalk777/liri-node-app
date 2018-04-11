0da32ea3e73eab8d35f1f1a699c5173e607e9879
Walker <kriswalker07@gmail.com>
2018-04-10 23:06:28 -0400

    Trying Times

---
+++ keys.js
@@ -0,0 +1,14 @@
+
+var twitterKeys = {
+	consumer_key: 'iBp7esjMRfcTBoxf7zPo3ev2y',
+	consumer_secret: 'A6RARuzpcTsDVZLEll37gwASF08mwhpZouWpYIaQIedATZexq2',
+	access_token_key: '983892120024092673-MQgC0iOZKaYP12pCKTG4ax4dgFpVug7',
+	access_token_secret: '05QX3Sz0fxIjjvooDciYtcvz5BOjYPkqkIQPe9TAC6aAh',
+};
+
+var spotifyKeys = {
+	id: '8cec9fe8404e4b27a3a5ec3aa05a3943',
+	secret: '5fa898d1e4e241c6903cc15a95e47606',
+};
+
+module.exports = { twitterKeys, spotifyKeys };
